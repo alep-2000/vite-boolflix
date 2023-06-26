f3af8616299d51c2688ecc97616fb81c786df5aa
alep-2000 <alessiopiras056@gmail.com>
2023-06-26 16:10:45 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+import axios from 'axios';
+
+
+export default {
+
+}
+</script>
+
+<template lang="">
+  <div>
+    
+  </div>
+</template>
+
+<style lang="scss">
+@use './style/generals.scss' as *;
+@use './style/partials/variables.scss' as *;
+@use './style/mixins.scss' as *;
+</style>
